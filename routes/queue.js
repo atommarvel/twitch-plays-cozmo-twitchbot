49cfc4408cb6745eaf3b146cd5d33d100d4fa695
Atom Raiff <atom.raiff@gmail.com>
2017-01-29 11:16:38 -0800

    give a none instruction when there is nothing in the queue

--- routes/queue.js
+++ routes/queue.js
@@ -7,7 +7,13 @@ function getRouter(queue) {
   // TODO: add a secret key queryparam so that people poking at the api can't start screwing up the queue
   /* GET next queued command */
   router.get('/', function(req, res, next) {
-      res.json(queue.shift());
+      var command = queue.shift();
+      if (!command) {
+        res.json({
+          "instruction": "!cc-none"
+        });
+      }
+      res.json();
   });
 
   return router;
